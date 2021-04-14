cd630ed0e24f8c96784bfc4ae5253e8579a29666
Stojan Kadiev <kadievstojan@gmail.com>
2021-04-14 13:48:42 +0200

    basic layout for projects

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import Button from './components/UI/Button/Button';
-import Modal from './components/UI/Modal/Modal';
 import Navigation from './components/Navigation/Navigation';
 import Members from './containers/Members/Members';
 import Projects from './containers/Projects/Projects';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Route, Switch } from 'react-router-dom';
 
 // show props and onHile
 function App() {
-  const [modalShow, setModalShow] = React.useState(false);
-
   const [navigationItems, setNavigationItems] = React.useState([
     { title: 'projects', href: '/projects' },
     { title: 'members', href: '/members' },
--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -1,13 +1,54 @@
 import React from 'react';
 import Modal from '../../components/UI/Modal/Modal';
 import Button from '../../components/UI/Button/Button';
+import { Row, Col, Container } from 'react-bootstrap';
 import ReactDOM from 'react-dom';
+import styles from './Projects.module.css';
+
+const projects = ['tote', 'koce', 'lile', 'dzole', 'boce'];
+
+const generateRow = (columnList, id) => {
+  return <Row key={id}>{columnList}</Row>;
+};
+
+const alignProjectRowsLayout = (projects) => {
+  const rowsOrderedList = [];
+
+  projects.forEach((projectName, index) => {
+    if (index + 1 === projects.length && index !== 0 && (index + 1) % 2 !== 0) {
+      rowsOrderedList.push(
+        generateRow(
+          <Col md={{ span: 6 }}>
+            <div className={styles.Projects}> {projectName}</div>
+          </Col>,
+          projectName + index
+        )
+      );
+    } else if ((index + 1) % 2 === 0 && index !== 0) {
+      rowsOrderedList[index - 1] = generateRow(
+        [
+          <Col key={projectName + (index - 1)}>
+            <div className={styles.Projects}> {projects[index - 1]}</div>
+          </Col>,
+          <Col key={projectName + index}>
+            <div className={styles.Projects}>{projectName}</div>
+          </Col>,
+        ],
+        projectName + index
+      );
+    }
+  });
+  return rowsOrderedList;
+};
+
 const Projects = (props) => {
   const [modalShow, setModalShow] = React.useState(false);
 
+  console.log(alignProjectRowsLayout(projects));
   return (
     <React.Fragment>
       <h1>Projects</h1>
+
       <Button
         variant="primary"
         clicked={() => setModalShow(true)}
@@ -23,6 +64,8 @@ const Projects = (props) => {
         </Modal>,
         document.getElementById('modal-content')
       )}
+
+      <Container fluid>{alignProjectRowsLayout(projects)}</Container>
     </React.Fragment>
   );
 };
